e2b9bfaaaf167c96f694d301769ac6ff830a9f7f
aleam007 <50541845+aleam007@users.noreply.github.com>
2024-02-24 23:48:08 +0700

    24_02_2024
    DONE:
    _Login
    _Remember
    _Get ID User

--- app/src/main/java/com/mongodb/tasktracker/LoginActivity.kt
+++ app/src/main/java/com/mongodb/tasktracker/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +14,19 @@ import io.realm.mongodb.AppConfiguration
 import io.realm.mongodb.Credentials
 import org.bson.Document
 
+
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
     private lateinit var app: App
+    private lateinit var rememberMeCheckBox: CheckBox
+
+    private val prefsName = "Final_Project"
+    private val usernameKey = "username"
+    private val passwordKey = "password"
+    private val rememberMeKey = "rememberMe"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +40,28 @@ class LoginActivity : AppCompatActivity() {
         usernameEditText = findViewById(R.id.input_username)
         passwordEditText = findViewById(R.id.input_password)
         loginButton = findViewById(R.id.button_login)
+        rememberMeCheckBox = findViewById(R.id.rememberMeCheckBox);
+        checkSavedCredentials()
+
 
         // Thiết lập sự kiện click cho nút đăng nhập
         loginButton.setOnClickListener {
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
             performLogin(username, password)
+        }
+    }
+    //Check app đã được đăng nhập trước đó và nhớ Acc chưa
+    private fun checkSavedCredentials() {
+        val settings = getSharedPreferences(prefsName, MODE_PRIVATE)
+        val savedUsername = settings.getString(usernameKey, "")
+        val savedPassword = settings.getString(passwordKey, "")
+        val rememberMe = settings.getBoolean(rememberMeKey, false)
+
+        if (rememberMe && !savedUsername.isNullOrEmpty() && !savedPassword.isNullOrEmpty()) {
+            usernameEditText.setText(savedUsername)
+            passwordEditText.setText(savedPassword)
+            rememberMeCheckBox.isChecked = true
         }
     }
 
@@ -58,7 +82,15 @@ class LoginActivity : AppCompatActivity() {
             if (result.isSuccess) {
 
                 Log.v("AUTH", "Successfully logged in.")
-                val intent = Intent(this, HomeActivity::class.java)
+                rememberAccount(username, password, rememberMeCheckBox.isChecked)
+                // Lấy User ID
+                val userId = app.currentUser()?.id
+
+                // Chuyển đến HomeActivity với User ID
+                val intent = Intent(this, HomeActivity::class.java).apply {
+                    putExtra("USER_ID", userId)
+                }
+
                 startActivity(intent)
 
                 // Kết thúc LoginActivity
@@ -71,4 +103,19 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+    private fun rememberAccount(username: String, password: String, rememberMe: Boolean) {
+        val settings = getSharedPreferences(prefsName, MODE_PRIVATE)
+        settings.edit().apply {
+            if (rememberMe) {
+                putString(usernameKey, username)
+                putString(passwordKey, password)
+            } else {
+                remove(usernameKey)
+                remove(passwordKey)
+            }
+            putBoolean(rememberMeKey, rememberMe)
+            apply()
+        }
+    }
+
 }
